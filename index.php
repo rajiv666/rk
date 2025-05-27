3f66f8ce0e978601619d9e1d60a9f82aecfa4cab
rajiv666 <160821302+rajiv666@users.noreply.github.com>
2025-05-27 17:13:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require 'db.php';
 $stmt = $pdo->query("SELECT * FROM users");
 $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-echo "<h1>Users</h1><ul>";
+echo "<h1>Users4</h1><ul>";
 foreach ($users as $user) {
     echo "<li>{$user['name']} ({$user['email']})</li>";
 }
